265d44ecde52e5d31d480719f4204f80ba0029d9
appuser <appuser@journey2-645d759449-lbbwp>
2023-04-04 22:32:47 +0000

    starter project

---
+++ client/public/script.js
@@ -0,0 +1,10 @@
+const loadEvent = function() {
+
+  // Write your JavaScript code after this line
+
+  
+  // Write your JavaScript code before this line
+
+}
+
+window.addEventListener("load", loadEvent);
