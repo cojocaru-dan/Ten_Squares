8eaf08b9c2351b089ab4c422d6b9692c6c8f9619
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-04-09 18:43:46 -0700

    Understand CSS basics

--- client/public/script.js
+++ client/public/script.js
@@ -1,8 +1,16 @@
 const loadEvent = function() {
 
   // Write your JavaScript code after this line
+  const rootElement = document.querySelector("body");
+  const divClone = document.querySelector("body > div:first-child").cloneNode(true);
+  rootElement.appendChild(divClone);
+    
+  // let currentElem = document.querySelector("body > div:last-child");
+  // for (let i = 0; i < 10; i++) {
+  //   currentElem.style.borderRadius = "50%";
+  //   currentElem = currentElem.firstElementChild;
+  // }
 
-  
   // Write your JavaScript code before this line
 
 }
